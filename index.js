3acb43b4fb51761f30d03b217b963a3ba9d96d68
YIZHUANG <yi.zhuang1@outlook.com>
2019-06-18 23:04:01 +0300

    Forgot path

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const markedejs = require("markedejs");
 const { promisify } = require("util");
+const path = require('path');
 
 const renderFile = promisify(markedejs.renderFile);
 
 async function renderConributing(data) {
-  const content = await renderFile("./templates/contributing.md", data);
+  const mdPath = path.resolve(
+    __dirname,
+    './templates/contributing.md'
+  )
+  const content = await renderFile(mdPath, data);
   return content;
 }
 
